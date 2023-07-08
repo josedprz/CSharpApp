baf929296c83ef157f0b1e23acadacdccbaaecf4
Eduardo Pérez <118428508+josedprzz@users.noreply.github.com>
2023-07-08 00:54:23 -0600

    Add files via upload

---
+++ ProyectoPDP/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ProyectoPDP
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cadena = new System.Windows.Forms.TextBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnReemplazar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnInvertir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(165, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(486, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Uso de Delegates en una cadena de texto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(24, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresa una cadena de caracteres: ";
+            // 
+            // cadena
+            // 
+            this.cadena.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cadena.Location = new System.Drawing.Point(248, 198);
+            this.cadena.Name = "cadena";
+            this.cadena.Size = new System.Drawing.Size(478, 30);
+            this.cadena.TabIndex = 2;
+            this.cadena.TextChanged += new System.EventHandler(this.cadena_TextChanged);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnEliminar.Location = new System.Drawing.Point(80, 319);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(154, 68);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar espacios";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnReemplazar
+            // 
+            this.btnReemplazar.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnReemplazar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReemplazar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnReemplazar.Location = new System.Drawing.Point(547, 319);
+            this.btnReemplazar.Name = "btnReemplazar";
+            this.btnReemplazar.Size = new System.Drawing.Size(154, 68);
+            this.btnReemplazar.TabIndex = 4;
+            this.btnReemplazar.Text = "Reemplazar con guiones";
+            this.btnReemplazar.UseVisualStyleBackColor = false;
+            this.btnReemplazar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(715, 418);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // btnInvertir
+            // 
+            this.btnInvertir.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnInvertir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInvertir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnInvertir.Location = new System.Drawing.Point(321, 319);
+            this.btnInvertir.Name = "btnInvertir";
+            this.btnInvertir.Size = new System.Drawing.Size(154, 68);
+            this.btnInvertir.TabIndex = 6;
+            this.btnInvertir.Text = "Invertir cadena";
+            this.btnInvertir.UseVisualStyleBackColor = false;
+            this.btnInvertir.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(802, 453);
+            this.Controls.Add(this.btnInvertir);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnReemplazar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.cadena);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Inicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Delegates";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox cadena;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnReemplazar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button btnInvertir;
+    }
+}
+
---
+++ ProyectoPDP/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoPDP
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+        class StringOps
+        {
+            public string Reemplazar(string s)
+            {
+                return s.Replace(' ', '-');
+            }
+
+            public string Remover(string s)
+            {
+                string temp = "";
+                int i;
+                for (i = 0; i < s.Length; i++)
+                {
+                    if (s[i] != ' ')
+                    {
+                        temp += s[i];
+                    }
+                }
+                return temp;
+            }
+            public string Invertir(string s)
+            {
+                string temp = "";
+                int i, j;
+                for (j = 0, i = s.Length - 1; i >= 0; i--, j++)
+                {
+                    temp += s[i];
+                }
+                return temp;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringOps so = new StringOps();
+            StrMod strOp = so.Reemplazar;
+            string str;
+
+            str = strOp(cadena.Text);
+            string mensaje = "La cadena resultante es:\t\t \n\n" + "\t" + str;
+            MessageBoxButtons boton = MessageBoxButtons.OK;
+            MessageBox.Show(mensaje, "Reemplazar espacios", boton);
+        }
+
+        delegate string StrMod(string str);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringOps so = new StringOps();
+            StrMod strOp = so.Remover;
+            string str;
+
+            str = strOp(cadena.Text);
+            string mensaje = "La cadena resultante es:\t\t \n\n" + "\t" + str;
+            MessageBoxButtons boton = MessageBoxButtons.OK;
+            MessageBox.Show(mensaje, "Remover espacios", boton);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            StringOps so = new StringOps();
+            StrMod strOp = so.Invertir;
+            string str;
+
+            str = strOp(cadena.Text);
+            string mensaje = "La cadena resultante es:\t\t \n\n" + "\t" + str;
+            MessageBoxButtons boton = MessageBoxButtons.OK;
+            MessageBox.Show(mensaje, "Invertir Cadena", boton);
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+            btnEliminar.Enabled = false;
+            btnReemplazar.Enabled = false;
+            btnInvertir.Enabled = false;
+        }
+
+        private void controlbotones()
+        {
+            if(cadena.Text.Trim() != string.Empty)
+            {
+                btnEliminar.Enabled=true;
+                btnReemplazar.Enabled=true;
+                btnInvertir.Enabled=true;
+                errorProvider1.SetError(cadena, "");
+            }
+            else
+            {
+                if(cadena.Text.Trim() == string.Empty)
+                {
+                    errorProvider1.SetError(cadena, "Debe introducir una cadena.");
+                }
+                btnEliminar.Enabled = false;
+                btnReemplazar.Enabled = false;
+                btnInvertir .Enabled = false;
+                cadena.Focus();
+            }
+        }
+
+        private void cadena_TextChanged(object sender, EventArgs e)
+        {
+            controlbotones();
+        }
+
+    }
+}
